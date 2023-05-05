0f837a3fa1b3e0c8f936012f691fa4442f27977e
nld91 <nathanleedonovan@gmail.com>
2023-05-05 22:45:35 +0000

    Initial commit

---
+++ nginx/bot/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>My Discord Bot</title>
+	<style>
+		body {
+			background-color: #36393F;
+			color: #DCDDDE;
+			font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+			font-size: 16px;
+			line-height: 1.4;
+			margin: 0;
+			padding: 0;
+		}
+
+		h1 {
+			color: #FFFFFF;
+			font-size: 36px;
+			font-weight: normal;
+			margin: 40px 0 20px;
+			text-align: center;
+		}
+
+		p {
+			margin: 0 0 20px;
+			text-align: center;
+		}
+
+		a {
+			color: #7289DA;
+			text-decoration: none;
+		}
+
+		a:hover {
+			text-decoration: underline;
+		}
+	</style>
+</head>
+<body>
+	<h1>My Discord Bot</h1>
+	<p>Check out my bot's code on <a href="https://github.com/nld91/discord_crypto_bot">GitHub</a></p>
+</body>
+</html>
---
+++ nginx/home/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>WELCOME TO NLD.LOL :D</title>
+	<style>
+		body {
+			background-color: #1F1F1F;
+			color: #FFFFFF;
+			font-family: Arial, sans-serif;
+			font-size: 16px;
+			line-height: 1.4;
+			margin: 0;
+			padding: 0;
+		}
+
+		h1 {
+			color: #FFFFFF;
+			font-size: 36px;
+			font-weight: normal;
+			margin: 40px 0 20px;
+			text-align: center;
+		}
+
+		p {
+			margin: 0 0 20px;
+			text-align: center;
+		}
+
+		a {
+			color: #FFFFFF;
+			text-decoration: none;
+		}
+
+		a:hover {
+			text-decoration: underline;
+		}
+
+		.container {
+			max-width: 800px;
+			margin: 0 auto;
+			padding: 40px;
+			text-align: center;
+		}
+
+		.button {
+			background-color: #7289DA;
+			color: #FFFFFF;
+			border-radius: 5px;
+			display: inline-block;
+			font-size: 20px;
+			font-weight: bold;
+			margin-top: 20px;
+			padding: 10px 20px;
+			text-decoration: none;
+			transition: background-color 0.3s ease;
+		}
+
+		.button:hover {
+			background-color: #677bc4;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<h1>Welcome to My Website</h1>
+		<p>This is my website, powered by nginx, php, and MariaDB. Check out my database with <a href="https://db.nld.lol">phpMyAdmin</a>.</p>
+		<a href="https://db.nld.lol" class="button">Go to phpMyAdmin</a>
+	</div>
+</body>
+</html>
